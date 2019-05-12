21fb7bf1c2e2f4172c45c596d2765541563bb141
Vladislav Kovdrya <kovdryavlad@gmail.com>
2019-05-12 22:18:07 +0300

    speed test of reading image

---
+++ ReadingTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using ImageProcessor;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> timeList = new List<double>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                DateTime t1 = DateTime.Now;
+                double[,,] arrayImage = BitmapConverter.BitmapToDoubleRgb(new Bitmap("testReading.png"));
+
+                DateTime t2 = DateTime.Now;
+                double time = (t2 - t1).TotalSeconds;
+
+                System.Diagnostics.Debug.WriteLine("reading"+i + ": " + time.ToString("0.0000"));
+
+                timeList.Add(time);
+            }
+
+            STAT stat = new STAT();
+            stat.SetData(timeList.ToArray());
+
+
+            System.Diagnostics.Debug.WriteLine("Expectation: " + stat.Expectation_niz.ToString("0.0000") + " | " + stat.Expectation.ToString("0.0000") + " | " + stat.Expectation_verh.ToString("0.0000"));
+            System.Diagnostics.Debug.WriteLine("Sigma: " + stat.Sigma_niz.ToString("0.0000") + " | " + stat.Sigma.ToString("0.0000") + " | " + stat.Sigma_verh.ToString("0.0000"));
+        }
+    }
+}
---
+++ ReadingTest/STAT.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTest
+{
+    public partial class STAT
+    {   
+        public double[] d;              //массив для изменения
+        
+        //для использования в двумерных данных
+        public void SetData(double[] input)
+        {
+            d = input;
+            CalcMainParams();
+        }
+
+        public void CalcMainParams()
+        {
+            GetKvantil();
+            CalcExpectation();
+            CalcDisp();
+            FillIntervalsForExpectation();
+            FillIntervalsForSigmas();
+        }
+        
+        //когда графики построены
+
+        //точечные оценки
+
+        public double Expectation;  //Мат ожидание
+        public double Dispersia;    //дисперсия
+        public double Sigma;        //среднее квадр. отклонение
+        
+        public void CalcExpectation() // математическое ожидание
+        {
+            double _result = 0;
+            for (int i = 0; i < d.Length; i++)
+                _result += d[i];
+            Expectation = _result / d.Length;
+        }
+
+        public void CalcDisp() //Дисперсия
+        {
+            double S = 0;
+            for (int i = 0; i < d.Length; i++)
+                S += Math.Pow((d[i] - Expectation), 2);
+
+            S /= d.Length - 1;
+
+            Dispersia = S;
+            Sigma = Math.Sqrt(S);
+        }       
+        //закончились точечные оценки
+
+        //интервальные оценки 
+        //Сигмы для интервальных оценок
+        public double Sigma_Expectation;       //для мат ожидания
+        public double Sigma_Dispersia;         //для дисперсии
+
+       
+        public void CalcSigma_Expectation()
+        {
+            Sigma_Expectation = Sigma / Math.Sqrt(d.Length);
+        }
+
+        public void CalcSigma_Dispersia()
+        {
+            Sigma_Dispersia = Sigma / Math.Sqrt(2 * d.Length);
+        }
+
+        //Оценки
+        //формат названия переменных - Параментр_нижняя тета,Параментр_верхняя тета
+        public double Expectation_niz;
+        public double Expectation_verh;
+
+        public double Sigma_niz;
+        public double Sigma_verh;
+        
+        public double gamma = 0.95;
+
+        public double _kvantil;
+
+        //интервальные оценки среднего //для StatNd
+        public void FillIntervalsForExpectation()
+        {
+            CalcSigma_Expectation();
+
+            Expectation_niz = Expectation - _kvantil * Sigma_Expectation;
+            Expectation_verh = Expectation + _kvantil * Sigma_Expectation;
+        }
+
+        //интервальные оценки среднего //для StatNd
+        public void FillIntervalsForSigmas()
+        {
+            CalcSigma_Dispersia();
+
+            Sigma_niz = Sigma - _kvantil * Sigma_Dispersia;
+            Sigma_verh = Sigma + _kvantil * Sigma_Dispersia;
+        }
+
+       private double GetKvantil()
+        {
+            double alpha = 1 - gamma;
+            
+            return Kvantili.Normal(alpha / 2);
+        }
+    }
+
+}
